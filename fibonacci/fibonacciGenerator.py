67357f1a1f0b894b7e6505503af278c105043e40
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-05-14 01:10:21 -0300

    Fibonacci passing on tests

--- fibonacci/fibonacciGenerator.py
+++ fibonacci/fibonacciGenerator.py
@@ -2,3 +2,4 @@ def generateFibonacciSequence(position: int) -> int:
     if position == 0 or position == 1:
         return position
     return generateFibonacciSequence(position - 1) + generateFibonacciSequence(position - 2)
+
--- fibonacci/fibonacciTest.py
+++ fibonacci/fibonacciTest.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 
 from fibonacci import fibonacciGenerator
 
